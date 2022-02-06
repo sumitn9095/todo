eef010dfc743f58554cbf2c24ea8b253e7f226d2
sumitn9095 <sumit.nirgude>
2022-02-06 19:54:30 +0530

    Simple - new build

--- dist/todo/common.8fe9493ff04bbcef.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktodo=self.webpackChunktodo||[]).push([[592],{9019:(u,e,o)=>{o.d(e,{N:()=>t});const t={production:!0,app_url:"https://sumit-todo-server.herokuapp.com/api/"}}}]);
